c48488eb1f0177018595cda7ea47fd479a911ee9
Oğuzhan Bay <oguzhan.bay@diyalog.im>
2022-01-11 18:31:45 +0300

    add fetch functionality

--- src/components/widgetLg/WidgetLg.jsx
+++ src/components/widgetLg/WidgetLg.jsx
@@ -1,79 +1,68 @@
-import "./widgetLg.css";
+import { LinearProgress } from '@material-ui/core';
+import moment from 'moment';
+import React, { useEffect, useState } from 'react';
+import './widgetLg.css';
 
 export default function WidgetLg() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const getUsers = async () => {
+    setLoading(true);
+    const res = await fetch(`https://dummyapi.io/data/v1/tag/water/post?limit=5`, {
+      method: 'GET',
+      headers: {
+        'app-id': '61bbe05888e226f27068d92e',
+      },
+    });
+    await res.json().then((res) => {
+      setUsers(res.data);
+      setLoading(false);
+    });
+  };
+
   const Button = ({ type }) => {
-    return <button className={"widgetLgButton " + type}>{type}</button>;
+    return <button className={'widgetLgButton ' + type}>{type}</button>;
   };
   return (
-    <div className="widgetLg">
-      <h3 className="widgetLgTitle">Latest transactions</h3>
-      <table className="widgetLgTable">
-        <tr className="widgetLgTr">
-          <th className="widgetLgTh">Customer</th>
-          <th className="widgetLgTh">Date</th>
-          <th className="widgetLgTh">Amount</th>
-          <th className="widgetLgTh">Status</th>
+    <div className='widgetLg'>
+      <h3 className='widgetLgTitle'>Latest transactions</h3>
+      <table className='widgetLgTable'>
+        <tr className='widgetLgTr'>
+          <th className='widgetLgTh'>Customer</th>
+          <th className='widgetLgTh'>Date</th>
+          <th className='widgetLgTh'>Amount</th>
+          <th className='widgetLgTh'>Status</th>
         </tr>
-        <tr className="widgetLgTr">
-          <td className="widgetLgUser">
-            <img
-              src="https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-              alt=""
-              className="widgetLgImg"
-            />
-            <span className="widgetLgName">Susan Carol</span>
-          </td>
-          <td className="widgetLgDate">2 Jun 2021</td>
-          <td className="widgetLgAmount">$122.00</td>
-          <td className="widgetLgStatus">
-            <Button type="Approved" />
-          </td>
-        </tr>
-        <tr className="widgetLgTr">
-          <td className="widgetLgUser">
-            <img
-              src="https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-              alt=""
-              className="widgetLgImg"
-            />
-            <span className="widgetLgName">Susan Carol</span>
-          </td>
-          <td className="widgetLgDate">2 Jun 2021</td>
-          <td className="widgetLgAmount">$122.00</td>
-          <td className="widgetLgStatus">
-            <Button type="Declined" />
-          </td>
-        </tr>
-        <tr className="widgetLgTr">
-          <td className="widgetLgUser">
-            <img
-              src="https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-              alt=""
-              className="widgetLgImg"
-            />
-            <span className="widgetLgName">Susan Carol</span>
-          </td>
-          <td className="widgetLgDate">2 Jun 2021</td>
-          <td className="widgetLgAmount">$122.00</td>
-          <td className="widgetLgStatus">
-            <Button type="Pending" />
-          </td>
-        </tr>
-        <tr className="widgetLgTr">
-          <td className="widgetLgUser">
-            <img
-              src="https://images.pexels.com/photos/4172933/pexels-photo-4172933.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-              alt=""
-              className="widgetLgImg"
-            />
-            <span className="widgetLgName">Susan Carol</span>
-          </td>
-          <td className="widgetLgDate">2 Jun 2021</td>
-          <td className="widgetLgAmount">$122.00</td>
-          <td className="widgetLgStatus">
-            <Button type="Approved" />
-          </td>
-        </tr>
+
+        {loading && (
+          <div className=''>
+            <LinearProgress color='secondary' className='circularProgress' />
+          </div>
+        )}
+        {!loading && (
+          <>
+            {users.map((user) => (
+              <tr className='widgetLgTr' key={user.id}>
+                <td className='widgetLgUser'>
+                  <img src={user.image} alt='' className='widgetLgImg' />
+                  <span className='widgetLgName'>
+                    {user.owner.firstName} {user.owner.lastName}
+                  </span>
+                </td>
+                <td className='widgetLgDate'> {moment(user.publishDate).format('MMM Do YY')} </td>
+                <td className='widgetLgAmount'>${user.likes * 1000} </td>
+                <td className='widgetLgStatus'>
+                  <Button type='Approved' />
+                </td>
+              </tr>
+            ))}
+          </>
+        )}
       </table>
     </div>
   );
--- src/components/widgetSm/WidgetSm.jsx
+++ src/components/widgetSm/WidgetSm.jsx
@@ -1,87 +1,60 @@
-import "./widgetSm.css";
-import { Visibility } from "@material-ui/icons";
+import './widgetSm.css';
+import { Visibility } from '@material-ui/icons';
+import { useEffect, useState } from 'react';
+import { CircularProgress } from '@material-ui/core';
 
 export default function WidgetSm() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const getUsers = async () => {
+    setLoading(true);
+    const res = await fetch(`https://dummyapi.io/data/v1/user?page=$1&limit=6`, {
+      method: 'GET',
+      headers: {
+        'app-id': '61bbe05888e226f27068d92e',
+      },
+    });
+    await res.json().then((res) => {
+      setUsers(res.data);
+      setLoading(false);
+    });
+  };
+
   return (
-    <div className="widgetSm">
-      <span className="widgetSmTitle">New Join Members</span>
-      <ul className="widgetSmList">
-        <li className="widgetSmListItem">
-          <img
-            src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500"
-            alt=""
-            className="widgetSmImg"
-          />
-          <div className="widgetSmUser">
-            <span className="widgetSmUsername">Anna Keller</span>
-            <span className="widgetSmUserTitle">Software Engineer</span>
-          </div>
-          <button className="widgetSmButton">
-            <Visibility className="widgetSmIcon" />
-            Display
-          </button>
-        </li>
-        <li className="widgetSmListItem">
-          <img
-            src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500"
-            alt=""
-            className="widgetSmImg"
-          />
-          <div className="widgetSmUser">
-            <span className="widgetSmUsername">Anna Keller</span>
-            <span className="widgetSmUserTitle">Software Engineer</span>
-          </div>
-          <button className="widgetSmButton">
-            <Visibility className="widgetSmIcon" />
-            Display
-          </button>
-        </li>
-        <li className="widgetSmListItem">
-          <img
-            src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500"
-            alt=""
-            className="widgetSmImg"
-          />
-          <div className="widgetSmUser">
-            <span className="widgetSmUsername">Anna Keller</span>
-            <span className="widgetSmUserTitle">Software Engineer</span>
-          </div>
-          <button className="widgetSmButton">
-            <Visibility className="widgetSmIcon" />
-            Display
-          </button>
-        </li>
-        <li className="widgetSmListItem">
-          <img
-            src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500"
-            alt=""
-            className="widgetSmImg"
-          />
-          <div className="widgetSmUser">
-            <span className="widgetSmUsername">Anna Keller</span>
-            <span className="widgetSmUserTitle">Software Engineer</span>
-          </div>
-          <button className="widgetSmButton">
-            <Visibility className="widgetSmIcon" />
-            Display
-          </button>
-        </li>
-        <li className="widgetSmListItem">
-          <img
-            src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&dpr=2&w=500"
-            alt=""
-            className="widgetSmImg"
-          />
-          <div className="widgetSmUser">
-            <span className="widgetSmUsername">Anna Keller</span>
-            <span className="widgetSmUserTitle">Software Engineer</span>
-          </div>
-          <button className="widgetSmButton">
-            <Visibility className="widgetSmIcon" />
-            Display
-          </button>
-        </li>
-      </ul>
+    <div className='widgetSm' style={{ position: 'relative' }}>
+      <span className='widgetSmTitle'>New Join Members</span>
+
+      {loading && (
+        <div style={{ position: 'absolute', left: '40%', top: '40%' }}>
+          <CircularProgress />
+        </div>
+      )}
+
+      {!loading && (
+        <ul className='widgetSmList'>
+          {users?.map((user) => (
+            <li className='widgetSmListItem' key={user.id}>
+              <img src={user.picture} alt='' className='widgetSmImg' />
+              <div className='widgetSmUser'>
+                <span className='widgetSmUsername'>
+                  {' '}
+                  {user.firstName} {user.lastName}{' '}
+                </span>
+                <span className='widgetSmUserTitle'>Software Engineer</span>
+              </div>
+              <button className='widgetSmButton'>
+                <Visibility className='widgetSmIcon' />
+                Display
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
