955dbe96e478d20d2101c14157e0123f207e701b
Oğuzhan Bay <oguzhan.bay@diyalog.im>
2022-01-11 17:38:43 +0300

    first commit

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./topbar.css";
+import { NotificationsNone, Language, Settings } from "@material-ui/icons";
+
+export default function Topbar() {
+  return (
+    <div className="topbar">
+      <div className="topbarWrapper">
+        <div className="topLeft">
+          <span className="logo">Admin</span>
+        </div>
+        <div className="topRight">
+          <div className="topbarIconContainer">
+            <NotificationsNone />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Language />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Settings />
+          </div>
+          <img src="https://res.cloudinary.com/db6kegyyc/image/upload/v1641408753/mv6kvfifiphpqi4egefa.jpg" alt="" className="topAvatar" />
+        </div>
+      </div>
+    </div>
+  );
+}
